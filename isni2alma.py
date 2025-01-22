e51e1497ccd1249c7455049f3435bf9cf30040fe
mikegsaunders <m.saunders@nls.uk>
2025-01-22 18:12:12 +0000

    added all fields to be passed into marc rec, added bib wrapper

--- isni2alma.py
+++ isni2alma.py
@@ -14,29 +14,51 @@ root = tree.getroot()
 
 for child in root:
     if child.tag == "record":
-        for subchild in child:
-            if subchild.tag == "controlfield":
-                if subchild.attrib["tag"] == "001":
-                    mmsID = subchild.text.strip()
-                    # probably a better way to do this bit:
-                    # if subchild.tag == "datafield":
-                    #     if subchild.attrib["tag"] == "100":
-                    #     etc
+        mmsID = child.find("controlfield[@tag='001']").text.strip()
+        LODs = child.findall("datafield[@tag='100']/subfield[@code='1']")
+        if child.find("datafield[@tag='100']/subfield[@code='z']") is not None:
+            qa = child.find("datafield[@tag='100']/subfield[@code='z']").text.strip()
+        else:
+            qa = ""
+        isni = ""
+        wiki = ""
+        for each in LODs:
+            if "isni" in each.text.strip():
+                isni = each.text.strip()
+            if "wiki" in each.text.strip():
+                wiki = each.text.strip()
+        # print(mmsID, isni, wiki, LOD_source)
+        # get alma rec from api
+        url = f"https://api-eu.hosted.exlibrisgroup.com/almaws/v1/bibs/{mmsID}?apikey={apiKey}"
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            # parse xml to pymarc
+            xml_content = r.content.decode("utf-8")
+            xml_file_like = StringIO(xml_content)
+            records = pymarc.parse_xml_to_array(xml_file_like)
 
-                    # get alma rec from api
-                    url = f"https://api-eu.hosted.exlibrisgroup.com/almaws/v1/bibs/{mmsID}?apikey={apiKey}"
-                    r = requests.get(url, headers=headers)
-                    if r.status_code == 200:
-                        xml_content = r.content.decode("utf-8")
-                        xml_file_like = StringIO(xml_content)
-                        records = pymarc.parse_xml_to_array(xml_file_like)
-                        for record in records:
-                            field100 = record.get("100")
-                            isni = "shisni"
-                            wiki = "shiski"
-                            field100_1 = field100.get_subfields("1")
-                            if "isni" not in field100_1:
-                                field100.addsubfield("1", isni)
-                            if "wiki" not in field100_1:
-                                field100.addsubfield("1", wiki)
-                            print(record["100"])
+            # add in linked open data fields
+            for record in records:
+                field100 = record.get("100")
+                field100_1 = field100.get_subfields("1")
+                if not any("isni" in subfield for subfield in field100_1):
+                    if isni != "":
+                        field100.add_subfield("1", isni)
+                    if qa != "":
+                        field100.add_subfield("z", qa)
+                else:
+                    field100.add_subfield("z", "ISNIQAPASS_HUMAN")
+                if not any("wiki" in subfield for subfield in field100_1):
+                    if wiki != "":
+                        field100.add_subfield("1", wiki)
+
+                # convert back to xml
+                xml = pymarc.record_to_xml(record)
+                # add bib wrapper
+                xml_element = ET.fromstring(xml)
+                bib_element = ET.Element("bib")
+                bib_element.append(xml_element)
+                xml_with_bib = ET.tostring(bib_element, encoding="utf-8")
+                print(xml_with_bib)
+                # send to alma
+                # r = requests.put(url, data=xml, headers=headers)
