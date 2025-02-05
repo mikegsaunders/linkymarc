1e7c200b33e39c34718644bcb2c5889447868c70
mikegsaunders <m.saunders@nls.uk>
2025-02-05 13:58:37 +0000

    added check for existing qa field

--- isni2alma.py
+++ isni2alma.py
@@ -8,9 +8,14 @@ from io import StringIO
 apiKey = os.environ.get("ALMA_API_SANDBOX")
 headers = {"Accept": "application/xml", "Content-Type": "application/xml"}
 
+# counters
+total = 0
+progress = 0
+
 
 tree = ET.parse("isni_wiki_v4_qa_test_alma_update.xml")
 root = tree.getroot()
+total = len(root)
 
 for child in root:
     if child.tag == "record":
@@ -29,10 +34,13 @@ for child in root:
                 wiki = each.text.strip()
         # skip if nothing new to add
         if isni == "" and qa == "":
+            progress += 1
             continue
         # get alma rec from api
         url = f"https://api-eu.hosted.exlibrisgroup.com/almaws/v1/bibs/{mmsID}?apikey={apiKey}"
         r = requests.get(url, headers=headers)
+        if r.status_code != 200:
+            print(r.text)
         if r.status_code == 200:
             # parse xml to pymarc
             xml_content = r.content.decode("utf-8")
@@ -43,13 +51,15 @@ for child in root:
             for record in records:
                 field100 = record.get("100")
                 field100_1 = field100.get_subfields("1")
+                field100_z = field100.get_subfields("z")
                 if not any("isni" in subfield for subfield in field100_1):
                     if isni != "":
                         field100.add_subfield("1", isni)
                     if qa != "":
                         field100.add_subfield("z", qa)
                 else:
-                    field100.add_subfield("z", "ISNIQAPASS_HUMAN")
+                    if len(field100_z) == 0:
+                        field100.add_subfield("z", "ISNIQAPASS_HUMAN")
                 if not any("wiki" in subfield for subfield in field100_1):
                     if wiki != "":
                         field100.add_subfield("1", wiki)
@@ -63,5 +73,7 @@ for child in root:
                 xml_with_bib = ET.tostring(bib_element, encoding="utf-8")
                 # send to alma
                 r = requests.put(url, data=xml_with_bib, headers=headers)
+                progress += 1
                 if r.status_code != 200:
                     print(r.text)
+    print(f"\rProgress: {progress}/{total} records", end="")
